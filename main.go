98afe2d346dc7230ed5c05a20dab8619cb561615
numan98khan <numan98khan@gmail.com>
2020-01-26 01:09:29 +0500

    Basic shell commands added (not functional yet)

--- main.go
+++ main.go
@@ -9,17 +9,68 @@ import (
 )
 
 func main() {
-  app := &cli.App{
-    Name: "GoJob",
-    Usage: "Jobs",
-    Action: func(c *cli.Context) error {
-      fmt.Println("Go Job Submission System")
-      return nil
-    },
-  }
+  	app := &cli.App{
+    	Name: "GoJob",
+    	Usage: "Jobs",
+    	Action: func(c *cli.Context) error {
+			fmt.Printf("Hello %q", c.Args().Get(0))
+			return nil
+		},
+		Commands: []*cli.Command{
+			{
+			  	Name:    "setup",
+			  	Usage:   "Sets up tej on a remote machine",
+			  	Action:  func(c *cli.Context) error {
+					return nil
+			  	},
+			},
+			{
+			  	Name:    "submit",
+			  	Usage:   "Submits a job to a remote machine",
+			  	Action:  func(c *cli.Context) error {
+					return nil
+			  	},
+			},
+			{
+				Name:    "status",
+				Usage:   "Gets the status of a job",
+				Action:  func(c *cli.Context) error {
+				  return nil
+				},
+			},
+			{
+				Name:    "download",
+				Usage:   "Downloads files from finished job",
+				Action:  func(c *cli.Context) error {
+				  return nil
+				},
+			},  
+			{
+				Name:    "kill",
+				Usage:   "Kills a running job",
+				Action:  func(c *cli.Context) error {
+				  return nil
+				},
+			},
+			{
+				Name:    "delete",
+				Usage:   "Deletes a finished job",
+				Action:  func(c *cli.Context) error {
+				  return nil
+				},
+			}, 
+			{
+				Name:    "list",
+				Usage:   "Lists remote jobs",
+				Action:  func(c *cli.Context) error {
+				  return nil
+				},
+		  	}, 
+		},
+  	}
 
-  err := app.Run(os.Args)
-  if err != nil {
-    log.Fatal(err)
-  }
+  	err := app.Run(os.Args)
+		if err != nil {
+			log.Fatal(err)
+  	}
 }
